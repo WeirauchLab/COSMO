f8af438644f8342824138d04041dafd9fa9dadad
Kevin Ernst <kevin.ernst@cchmc.org>
2017-12-05 02:04:32 -0500

    Allow reusing coords/counts from previous run
    (for testing, mostly)

--- example.sh
+++ example.sh
@@ -10,6 +10,9 @@ BGSCANS=${BGSCANS:-100}
 DISTANCE=${DISTANCE:-10}
 THRESHOLD=${THRESHOLD:-0.6}
 COSMOARGS="-fa ./example.fa -t $THRESHOLD -d $DISTANCE -p ./jpwm/"
+
+# reuse existing cosmo.coords.bed and cosmo.counts.tab?
+REUSE=${REUSE:-}
 DEBUG=${DEBUG:-}
 LOGTAIL=${LOGTAIL:-5}
 LOGDIR=${LOGDIR:-$MYDIR/log}
@@ -40,6 +43,14 @@ silently() { "$@" &>/dev/null; }
 # just suppress stderr
 quietly() { "$@" 2>/dev/null; }
 
+is_set() {
+    if [[ $1 =~ ^(y|Y|[Tt][Rr][Uu]) || $1 -gt 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 killp() {
     # don't die in the middle of the die handler
     set +ex
@@ -55,7 +66,7 @@ cleanup() {
     set +ex
     local sig=$1; shift
     echo -ne "\n${RESET}${MAGENTA}[Caught $sig]:${RESET} " >&2
-    if [[ $DEBUG =~ ^(y|Y|[Tt][Rr][Uu]) || $DEBUG -gt 0 ]]; then 
+    if is_set "$DEBUG"; then 
         echo -ne "${YELLOW}DEBUG mode set; not touching " >&2
         echo "'$LOGDIR'${RESET}" >&2
     else
@@ -99,66 +110,73 @@ fi
 
 export PYTHONPATH=$PYTHONPATH:./MOODS/python/build/lib.$pythonver
 
-echo -e "\n${BOLD}Running COSMO analyses...${RESET}\n"
+# Don't regenerate the counts/coords files if REUSE=yes/true/1
+if is_set "$REUSE" && [ -f 'cosmo.coords.bed' -a -f 'cosmo.counts.tab' ]; then
+    echo -ne "\n${YELLOW}REUSE mode set; using existing results "
+    echo -e "('cosmo.coords.bed' and 'cosmo.counts.tab')${RESET}"
+else
+    echo -e "\n${BOLD}Running COSMO analyses...${RESET}\n"
 
-# extract the sample FASTA if necessary
-test -f ./example.fa || gzip -dc < example.fa.gz > example.fa
+    # extract the sample FASTA if necessary
+    test -f ./example.fa || gzip -dc < example.fa.gz > example.fa
 
-# semicolon causes a syntax error after a '&'; see https://tf.cchmc.org/s/c2mri
-set -x
-./cosmo_v1.py $COSMOARGS &> "$LOGDIR/bgtask1.log"    & pids[0]=$!
-./cosmo_v1.py $COSMOARGS &> "$LOGDIR/bgtask2.log"    & pids[1]=$!
-./cosmo_v1.py $COSMOARGS -C &> "$LOGDIR/bgtask3.log" & pids[2]=$!
+    # semicolon causes a syntax error after a '&'; see https://tf.cchmc.org/s/c2mri
+    set -x
+    ./cosmo_v1.py $COSMOARGS &> "$LOGDIR/bgtask1.log"    & pids[0]=$!
+    ./cosmo_v1.py $COSMOARGS &> "$LOGDIR/bgtask2.log"    & pids[1]=$!
+    ./cosmo_v1.py $COSMOARGS -C &> "$LOGDIR/bgtask3.log" & pids[2]=$!
 
-# now, trap CTRL+C to kill off background processes before we exit
-trap "killp SIGINT ${pids[*]}; exit 1" SIGINT
+    # now, trap CTRL+C to kill off background processes before we exit
+    trap "killp SIGINT ${pids[*]}; exit 1" SIGINT
 
-# wait a sec before turning off trace so all the bg jobs show up in the output
-sleep 1
-set +x
+    # wait a sec before turning off trace so all the bg jobs show up in the output
+    sleep 1
+    set +x
 
-echo -ne "\n${BOLD}${UL}${YELLOW}NOTE${RESET}: ${BOLD}If they misbehave, you "
-echo -e "can terminate these jobs with the command${RESET}\n"
-echo -e "      ${BOLD}${BLUE}kill ${pids[*]}${RESET}\n"
+    echo -ne "\n${BOLD}${UL}${YELLOW}NOTE${RESET}: ${BOLD}If they misbehave, you "
+    echo -e "can terminate these jobs with the command${RESET}\n"
+    echo -e "      ${BOLD}${BLUE}kill ${pids[*]}${RESET}\n"
 
-read -t 5 -p "${BOLD}Press ENTER to continue (or wait 5s)...${RESET} " JUNK \
-    || true  # because of the 'set -e' above
+    read -t 5 -p "${BOLD}Press ENTER to continue (or wait 5s)...${RESET} " JUNK \
+        || true  # because of the 'set -e' above
 
-# tail logfiles while we're waiting for the background jobs to finish
-elapsed=0
+    # tail logfiles while we're waiting for the background jobs to finish
+    elapsed=0
+    j=
 
-while (( 1 )); do
-    clear
-    echo -ne "${BOLD}Checking every 5s for completion of "
-    echo -ne "PIDs ${BLUE}${pids[*]}${WHITE}...${RESET} "
-    echo -e "(${YELLOW}CTRL+C to quit${RESET})\n"
+    while (( 1 )); do
+        clear
+        echo -ne "${BOLD}Checking every 5s for completion of "
+        echo -ne "PIDs ${BLUE}${pids[*]}${WHITE}...${RESET} "
+        echo -e "(${YELLOW}CTRL+C to quit${RESET})\n"
 
-    echo -n "${UL}Last $LOGTAIL lines of background task #1, 2, 3 log files; "
-    echo -e "elapsed time $(( elapsed/60 ))m${RESET}\n"
+        echo -n "${UL}Last $LOGTAIL lines of background task #1, 2, 3 log files; "
+        echo -e "elapsed time $(( elapsed/60 ))m${RESET}\n"
 
-    # suppress column's griping about 'line too long'
-    quietly column -c120 <(quietly tail -$LOGTAIL "$LOGDIR/bgtask1.log") \
-                         <(quietly tail -$LOGTAIL "$LOGDIR/bgtask2.log") \
-                         <(quietly tail -$LOGTAIL "$LOGDIR/gbtask3.log") || true
+        # suppress column's griping about 'line too long'
+        quietly column -c120 <(quietly tail -$LOGTAIL "$LOGDIR/bgtask1.log") \
+                             <(quietly tail -$LOGTAIL "$LOGDIR/bgtask2.log") \
+                             <(quietly tail -$LOGTAIL "$LOGDIR/gbtask3.log") || true
 
-    sleep 5
-    elapsed=$(( elapsed+=5 ))
+        sleep 5
+        elapsed=$(( elapsed+=5 ))
 
-    # keep the outer (while) loop going unless /none/ of the PIDs are found
-    for j in {1..3}; do silently ps -p ${pids[$j]} && continue 2; done
+        # keep the outer (while) loop going unless /none/ of the PIDs are found
+        for j in {1..3}; do silently ps -p ${pids[$j]} && continue 2; done
 
-    # otherwise no more background jobs; untrap CTRL+C and get out
-    trap - 2
-    break
-done
+        # otherwise no more background jobs; untrap CTRL+C and get out
+        trap - 2
+        break
+    done
+fi # if REUSE was set and cosmo.coords.bed and cosmo.counts.tab exist
 
 echo
 
 # brace expansion happens before variable interpolation (so 'for i in {1..$var}'
 # won't work), need an arithmetic 'for' loop; see https://tf.cchmc.org/s/zvu1t
 for (( i = 1; i <= $BGSCANS; i++ )); do
-    echo -e "${BOLD}Running background scan iteration #$i / "
-    echo "$BGSCANS${RESET}... "
+    echo -ne "${BOLD}Running background scan iteration #$i / "
+    echo -n "$BGSCANS${RESET}... "
 
     echo "==== Commencing scan iteration #$i/$BGSCANS at $(date -R)" \
         >>"$LOGDIR/bgscans.log"
@@ -168,7 +186,7 @@ for (( i = 1; i <= $BGSCANS; i++ )); do
     echo "==== Finished iteration #$i/$BGSCANS at $(date -R)" \
         >>"$LOGDIR/bgscans.log"
 
-    echo -e "${GREEN}done.${RESET}"
+    echo "${GREEN}done.${RESET}"
 done
 
 echo -ne "\n${BOLD}Collecting stats...${RESET} "
